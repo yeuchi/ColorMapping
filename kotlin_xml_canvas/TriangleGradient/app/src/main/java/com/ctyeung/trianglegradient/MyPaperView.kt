0fb38827fc60f94fcae480fba5077ac5e29c062c
Chi Yeung <yeun0007@umn.edu>
2024-06-18 17:56:41 -0500

    Work in progress : functional rendering
    a. need to fix coordinates
    b. need to fix triangle

--- kotlin_xml_canvas/TriangleGradient/app/src/main/java/com/ctyeung/trianglegradient/MyPaperView.kt
+++ kotlin_xml_canvas/TriangleGradient/app/src/main/java/com/ctyeung/trianglegradient/MyPaperView.kt
@@ -22,6 +22,7 @@ import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
 import androidx.core.graphics.alpha
+import androidx.core.graphics.blue
 import androidx.core.graphics.green
 import androidx.core.graphics.red
 
@@ -32,7 +33,7 @@ class MyPaperView(
 ) :
     View(context, attrs) {
     val dotColor = Color.BLUE
-    val lineColor = Color.argb(255, 0, 255, 0)
+    val lineColor = Color.argb(255, 0, 0, 0)
 
     // defines paint and canvas
     var drawPaint: Paint? = null
@@ -48,6 +49,8 @@ class MyPaperView(
     }
 
     override fun onDraw(canvas: Canvas) {
+       // points = arrayListOf<PointF>(PointF(42f, 269f), PointF(134f, 131f), PointF(289f, 262f))
+
         bmp?.let {
             drawKnots(canvas)
             drawTriangle(canvas)
@@ -98,9 +101,9 @@ class MyPaperView(
                 path.close()
                 canvas.drawPath(path, drawPaint!!)
                 bmp?.let {
-                    canvas.drawBitmap(it, null, Rect(0, 0, it.width, it.height), null)
+                    val rect = Rect(0, 0, it.width, it.height)
+                    canvas.drawBitmap(it, null, rect, null)
                 }
-
                 fillGradientColors(canvas)
             }
 
@@ -120,8 +123,8 @@ class MyPaperView(
         bmp?.let {
             val mtx = createMatrix(
                 points,       // [in] array of triangle reference positions
-                Color.RED,    // [in] triangle reference color 0
-                Color.GREEN,  // [in] triangle reference color 1
+                Color.GREEN,    // [in] triangle reference color 0
+                Color.RED,  // [in] triangle reference color 1
                 Color.BLUE    // [in] triangle reference color 2
             )
 
@@ -131,12 +134,14 @@ class MyPaperView(
                 for (x in 0 until width) {
                     /* if pixel in triangle */
                     val pixel = it.getPixel(x,y)
-//                    if(pixel.green == 255) {
-                        val b = requant(mtx[0] * x + mtx[1] * y + mtx[2])
-                        val g = requant(mtx[3] * x + mtx[4] * y + mtx[5])
-                        val r = requant(mtx[6] * x + mtx[7] * y + mtx[8])
-                        val color: Int = Color.argb(255, r, g, b)
-                        it.setPixel(x, y, color)
+//                    pixel.apply {
+//                        if(green == 0 && red == 0 && blue == 0 && alpha == 255) {
+                            val b = requant(mtx[0] * x + mtx[1] * y + mtx[2])
+                            val g = requant(mtx[3] * x + mtx[4] * y + mtx[5])
+                            val r = requant(mtx[6] * x + mtx[7] * y + mtx[8])
+                            val color: Int = Color.argb(255, r, g, b)
+                            it.setPixel(x, y, color)
+//                        }
 //                    }
                 }
             }
@@ -159,7 +164,7 @@ class MyPaperView(
      * https://developer.android.com/reference/kotlin/android/graphics/Color
      */
     private fun createMatrix(
-        points: ArrayList<PointF>,
+        pointList: ArrayList<PointF>,
         color1: Int,
         color2: Int,
         color3: Int,
@@ -168,17 +173,17 @@ class MyPaperView(
          * TODO Use 4x4 matrix
          *  https://developer.android.com/reference/android/opengl/Matrix
          */
+
         val dMtx = arrayListOf<Double>(
-            points[0].x.toDouble(), points[0].y.toDouble(), 1.0,
-            points[1].x.toDouble(), points[1].y.toDouble(), 1.0,
-            points[2].x.toDouble(), points[2].y.toDouble(), 1.0
+            pointList[0].x.toDouble(), pointList[0].y.toDouble(), 1.0,
+            pointList[1].x.toDouble(), pointList[1].y.toDouble(), 1.0,
+            pointList[2].x.toDouble(), pointList[2].y.toDouble(), 1.0
         )
 
         // inverse matrix
-        val det = dMtx[0] * (dMtx[4] * dMtx[8] - dMtx[5] * dMtx[7])
-        -dMtx[1] * (dMtx[3] * dMtx[8] - dMtx[5] * dMtx[6])
-        +dMtx[2] * (dMtx[3] * dMtx[7] - dMtx[4] * dMtx[6])
+        val det = dMtx[0] * (dMtx[4] * dMtx[8] - dMtx[5] * dMtx[7]) - dMtx[1] * (dMtx[3] * dMtx[8] - dMtx[5] * dMtx[6]) + dMtx[2] * (dMtx[3] * dMtx[7] - dMtx[4] * dMtx[6])
 
+        // det = 33442
         var iMtx = arrayListOf<Double>(
             0.0, 0.0, 0.0,
             0.0, 0.0, 0.0,
@@ -193,6 +198,10 @@ class MyPaperView(
         iMtx[2] = (dMtx[1] * dMtx[5] - dMtx[2] * dMtx[4]) / det;
         iMtx[5] = (dMtx[2] * dMtx[3] - dMtx[0] * dMtx[5]) / det;
         iMtx[8] = (dMtx[0] * dMtx[4] - dMtx[1] * dMtx[3]) / det;
+
+//        (-0.003917229830751749, -0.0002093176245439866, 0.0041265474552957355,
+//        0.004634890257759703, -0.007385921894623527, 0.002751031636863824,
+//        -0.08226182644578674, 1.9956043298845763, -0.9133425034387895)
 
         // color matrix
         val red1 = Color.red(color1)
@@ -225,6 +234,9 @@ class MyPaperView(
         mtx[7] = red3 * iMtx[3] + grn3 * iMtx[4] + blu3 * iMtx[5]
         mtx[8] = red3 * iMtx[6] + grn3 * iMtx[7] + blu3 * iMtx[8]
         return mtx
+//       -0.053375994258716586, -1.8834100831289995, 508.8791041205669,
+//        -0.998893606841696, 1.1818970157287243, -20.976765743675617,
+//        1.0522696011004125, 0.7015130674002751, -232.90233837689132
     }
 
     fun clear() {
