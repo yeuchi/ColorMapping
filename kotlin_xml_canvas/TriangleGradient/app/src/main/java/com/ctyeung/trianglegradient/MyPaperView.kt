65a51f3b828e504bdadcdc93ec5fac699c24c5d4
Chi Yeung <yeun0007@umn.edu>
2024-06-22 15:04:21 -0500

    Lint : clean up
    a. documentation
    b. evaluate only pixels within rect (triangle bound)
    c. clear bitmap when user deletes

--- kotlin_xml_canvas/TriangleGradient/app/src/main/java/com/ctyeung/trianglegradient/MyPaperView.kt
+++ kotlin_xml_canvas/TriangleGradient/app/src/main/java/com/ctyeung/trianglegradient/MyPaperView.kt
@@ -32,15 +32,14 @@ class MyPaperView(
     attrs: AttributeSet
 ) :
     View(context, attrs) {
-    val dotColor = Color.BLUE
-    val lineColor = Color.argb(255, 0, 0, 0)
+    private val dotColor = Color.BLUE
+    private val lineColor = Color.argb(255, 0, 0, 0)
 
     // defines paint and canvas
-    var drawPaint: Paint? = null
-    var path: Path? = null
-    var points = arrayListOf<PointF>()
-    var bmp: Bitmap? = null
-    var myCanvas: Canvas? = null
+    private var path: Path? = null
+    private var points = arrayListOf<PointF>()
+    private var bmp: Bitmap? = null
+    private var canvasMask: Canvas? = null
 
     init {
         isFocusable = true
@@ -48,6 +47,11 @@ class MyPaperView(
         path = Path()
     }
 
+    /**
+     * View draw call
+     * - initialize bmp and canvasMask if 1st time
+     * - draw point(s) and triangle otherwise
+     */
     override fun onDraw(canvas: Canvas) {
         /*
          * For debugging matrix calculation
@@ -62,11 +66,14 @@ class MyPaperView(
             val width = canvas.width
             bmp = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
             bmp?.let {
-                myCanvas = Canvas(it)
+                canvasMask = Canvas(it)
             }
         }
     }
 
+    /**
+     * Draw a circular dot for each point
+     */
     private fun drawKnots(canvas: Canvas) {
         val dotPaint = Paint()
         dotPaint.style = Paint.Style.FILL
@@ -79,52 +86,64 @@ class MyPaperView(
         }
     }
 
+    /**
+     * Draw line(s) or a triangle
+     * - if a triangle, fill it
+     */
     private fun drawTriangle(canvas: Canvas) {
-        drawPaint = Paint()
-        drawPaint!!.isAntiAlias = true
-        drawPaint!!.strokeWidth = 3f
-        drawPaint!!.style = Paint.Style.FILL_AND_STROKE
-        drawPaint!!.strokeJoin = Paint.Join.ROUND
-        drawPaint!!.strokeCap = Paint.Cap.ROUND
-        drawPaint!!.color = lineColor
+        Paint().let { drawPaint ->
+            drawPaint.isAntiAlias = true
+            drawPaint.strokeWidth = 3f
+            drawPaint.style = Paint.Style.FILL_AND_STROKE
+            drawPaint.strokeJoin = Paint.Join.ROUND
+            drawPaint.strokeCap = Paint.Cap.ROUND
+            drawPaint.color = lineColor
 
-        val path = Path()
+            val path = Path()
 
-        when {
-            points.size == 3 -> {
-                path.fillType = Path.FillType.EVEN_ODD
-                points[0].apply {
-                    path.moveTo(x, y)
+            when {
+                points.size == 3 -> {
+                    path.fillType = Path.FillType.EVEN_ODD
+                    points[0].apply {
+                        path.moveTo(x, y)
+                    }
+
+                    points[1].apply {
+                        path.lineTo(x, y)
+                    }
+
+                    points[2].apply {
+                        path.lineTo(x, y)
+                    }
+                    path.close()
+                    canvas.drawPath(path, drawPaint)
+
+                    /* draw on bitmap -- create a mask for fillGradient */
+                    canvasMask?.drawPath(path, drawPaint)
+
+                    /* fill above triangle with gradient colors */
+                    fillGradientColors(canvas)
                 }
 
-                points[1].apply {
-                    path.lineTo(x, y)
+                else -> {
+                    for (i in 0..points.size - 2) {
+                        val p = points[i]
+                        val pp = points[i + 1]
+                        path.moveTo(p.x.toFloat(), p.y.toFloat())
+                        path.lineTo(pp.x.toFloat(), pp.y.toFloat())
+                    }
+                    canvas.drawPath(path, drawPaint)
                 }
-
-                points[2].apply {
-                    path.lineTo(x, y)
-                }
-                path.close()
-                canvas.drawPath(path, drawPaint!!)
-
-                /* draw on bitmap */
-                myCanvas?.drawPath(path, drawPaint!!)
-
-                fillGradientColors(canvas)
-            }
-
-            else -> {
-                for (i in 0..points.size - 2) {
-                    val p = points[i]
-                    val pp = points[i + 1]
-                    path.moveTo(p.x.toFloat(), p.y.toFloat())
-                    path.lineTo(pp.x.toFloat(), pp.y.toFloat())
-                }
-                canvas.drawPath(path, drawPaint!!)
             }
         }
     }
 
+    /**
+     * Fill triangle with gradient color
+     * a. define 3x3 matrix for 3 points boundary colors
+     * b. get rectangle containing triangle
+     * c. test if pixel is in triangle -> calculate pixel color; else ignore
+     */
     private fun fillGradientColors(canvas: Canvas) {
         bmp?.let {
             val mtx = Matrix().create3x3(
@@ -135,15 +154,15 @@ class MyPaperView(
             )
 
             /* Draw transparent bitmap */
-            val rect = Rect(0, 0, width, height)
-            for (y in 0 until height) {
-                for (x in 0 until width) {
+            val rect = findRectBoundTriangle()
+            for (y in rect.top until rect.bottom) {
+                for (x in rect.left until rect.right) {
                     /*
                      * TODO use line formula to determine pixels inside triangle
                      */
-                    /* if pixel in triangle */
                     val pixel = it.getPixel(x, y)
                     pixel.apply {
+                        /* if pixel in triangle */
                         if (green == 0 && red == 0 && blue == 0 && alpha == 255) {
                             val b = requantize(mtx[0] * x + mtx[1] * y + mtx[2])
                             val g = requantize(mtx[3] * x + mtx[4] * y + mtx[5])
@@ -156,10 +175,38 @@ class MyPaperView(
             }
 
             /* draw bitmap into canvas */
-            canvas.drawBitmap(it, null, rect, null)
+            canvas.drawBitmap(it, null, Rect(0,0,it.width, it.height), null)
         }
     }
 
+    /**
+     * Find Rect that bounds over triangle (3) points
+     * - so we don't iterate over other pixels
+     */
+    private fun findRectBoundTriangle():Rect {
+        val upLeft = PointF(10000F, 10000F)
+        val btmRight = PointF(0F, 0F)
+
+        points.forEach{
+            if(it.x < upLeft.x) {
+                upLeft.x = it.x
+            }
+            if(it.y < upLeft.y) {
+                upLeft.y = it.y
+            }
+            if(it.x > btmRight.x) {
+                btmRight.x = it.x
+            }
+            if(it.y > btmRight.y) {
+                btmRight.y = it.y
+            }
+        }
+        return Rect(upLeft.x.toInt(), upLeft.y.toInt(), btmRight.x.toInt(), btmRight.y.toInt())
+    }
+
+    /**
+     * Clam pixel color value to 0 - 255
+     */
     private fun requantize(num: Double): Int {
         return when {
             num < 0 -> 0
@@ -168,14 +215,19 @@ class MyPaperView(
         }
     }
 
-
-
+    /**
+     * Clear rendering
+     */
     fun clear() {
         points.clear()
+        bmp?.eraseColor(Color.TRANSPARENT)
 //        path = Path()
         postInvalidate() // Indicate view should be redrawn
     }
 
+    /**
+     * Touch event -> create point(s)
+     */
     override fun onTouchEvent(event: MotionEvent): Boolean {
         val point = PointF(event.x, event.y)
 
